d74fc881cf6a7cf2e177af5c159fb41dae1e236f
Dastan-101 <145772344+Dastan-101@users.noreply.github.com>
2023-09-23 10:42:40 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -21,7 +21,7 @@
                     partials: ["REACTION"]
                 });
 
-                await s4d.client.login('MTE1NDQzMTE5MjM3Mjg3MTI5OQ.G0sKMk.lyszzRaHCAlHnLjuMmyXnmBSvoyBKKAOoKSqf8').catch((e) => { s4d.tokenInvalid = true; s4d.tokenError = e; });
+                await s4d.client.login('MTE1NDQzMTE5MjM3Mjg3MTI5OQ.GrDXGa.QiL1xtuVAWrAwYM9ohT-d2rUdcqIw8AMqMSbnw').catch((e) => { s4d.tokenInvalid = true; s4d.tokenError = e; });
 
 s4d.client.on('messageCreate', async (s4dmessage) => {
   if ((s4dmessage.content) == '!alvaro 0') {
